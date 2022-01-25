d6608469a61a69c7a7967b820c197ef751239d71
wsl_Dyann <wiltestrun458@gmail.com>
2022-01-25 21:28:22 +0800

    Add send hit function

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -1,16 +1,7 @@
 export default function GameBoard() {
   const body = new Array(10).fill(new Array(10).fill(null));
   const shipArray = [];
-  //   const checkShipAvailability = (col,row,isVertical,ship)=>{
-  //     for (let i = 0; i < ship.length; i += 1) {
-  //         if (isVertical) {
-  //             body[row + i][col]
-
-  //         } else {
-
-  //         }
-  //       }
-  //   };
+  const _missedAttacks = [];
   const shipCoordinates = (col, row, isVertical, length) => {
     const arr = [];
     for (let i = 0; i < length; i += 1) {
@@ -19,40 +10,41 @@ export default function GameBoard() {
       // } else {
       //   arr.push([row,col+i])
       // }
-      arr.push(isVertical ? [col, row + i] : [col + i, row]);
+      arr.push(isVertical ? [row + i, col] : [row, col + i]);
     }
     return arr;
   };
   const placeShip = ({ x: col, y: row, isVertical }, ship) => {
-    // * Check if there is already another ship there
     const coordinates = shipCoordinates(col, row, isVertical, ship.length);
-    const isAvailable = coordinates.every(([x, y]) => !body[y][x]);
+    // * Check if there is already another ship there
+    const isAvailable = coordinates.every(([y, x]) => !body[y][x]);
     if (!isAvailable) return false;
     coordinates.forEach((coord) => {
-      const [col, row] = coord;
-      body[row][col] = true;
+      const [y, x] = coord;
+      body[y][x] = true;
     });
-    return true;
-    // const coordinates = [];
-    // for (let i = 0; i < ship.length; i += 1) {
-    //   if (isVertical) {
-    //     body[row + i][col] = true;
-    //     coordinates.push([row + i, col]);
-    //   } else {
-    //     body[row][col + i] = true;
-    //     coordinates.push([row, col + i]);
-    //   }
-    // }
-    // shipArray.push({ coordinates, ship });
+    shipArray.push({ coordinates, ship });
     return true;
   };
   const recieveAttack = (coor) => {
     const [row, col] = coor;
     if (!body[row][col]) {
+      // record the coordinate of missed shot
+      _missedAttacks.push([row, col]);
       return false;
     }
-    // record the coordinate of missed shot
     // send hit function to ship
+    let index = null;
+    const myShip = shipArray.find((obj) =>
+      obj.coordinates.find(([y, x], idx) => {
+        if (y === coor[0] && x === coor[1]) {
+          index = idx;
+          return true;
+        }
+      })
+    );
+    console.log(myShip.coordinates[index]);
+    myShip.ship.hit(index);
     // check if already been placed/missed same spot
     return true;
   };
