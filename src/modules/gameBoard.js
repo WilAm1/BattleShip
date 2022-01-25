9b47972e9ae9405fefa9329d5065c0ceae0bd2f8
wsl_Dyann <wiltestrun458@gmail.com>
2022-01-25 08:15:28 +0800

    Fix placeShip forloop

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -2,8 +2,8 @@ export default function GameBoard() {
   const body = new Array(10).fill(new Array(10).fill(null));
   const shipArray = [];
   const placeShip = ({ x, y, isVertical }, ship) => {
-    for (let i = x; i < ship.length; i += 1) {
-      body[y][i] = true;
+    for (let i = 0; i < ship.length; i += 1) {
+      body[y][x + i] = true;
     }
     shipArray.push(ship);
   };
