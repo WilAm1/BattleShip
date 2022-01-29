5fc2652f18ee488ba48cfd27204eb4c4a3f88348
wsl_Dyann <wiltestrun458@gmail.com>
2022-01-29 10:10:20 +0800

    Add working game without proper UI

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,16 +1,11 @@
 /* 
-* Refactor (Marking functions)
-* Add Ending Game and remove alerts
-* Add Initialize ships
-* Display Own Ships
-TODO Add Randomize enemy ships
-TODO Inherit Player Class Computer Class
+TODO Finish up the UI
+TODO Refactor
 */
 
 import generateBoard from "./components/board";
 import Player from "./Player";
 import Ship from "./ship";
-import GameBoard from "./gameBoard";
 
 const hasNoMovesLeft = (cell) => {
   return !cell.parentElement.querySelectorAll(".active").length;
@@ -21,7 +16,7 @@ const createModal = () => {
   modal.classList.add("modal");
   return modal;
 };
-// * Removes child using parentElement.remoevChild and appendChild
+
 const onDrag = (e) => {
   const message = JSON.stringify(e.target.dataset);
   e.dataTransfer.setData("text/plain", message);
@@ -31,10 +26,7 @@ const onDrag = (e) => {
 const onDragEnd = (e) => {
   e.target.style.opacity = 1;
 };
-const onDrop = (e) => {
-  e.preventDefault();
-  const data = e.dataTransfer.getData("text");
-};
+
 const makeShipElement = ([shipName, length]) => {
   const element = document.createElement("div");
   element.classList.add("ship");
@@ -58,11 +50,34 @@ const shipCoordinates = (col, row, isVertical, length) => {
   }
   return arr;
 };
+const getRandomCoordinate = (len) => {
+  return Math.floor(Math.random() * len);
+};
 
-const openingModal = (player, board) => {
-  //initiliaze modal
+// Places randomized coordinates on enemy Ships
+const placeRandomShips = (gameBoard, ships) => {
+  Object.values(ships).forEach((shipLength) => {
+    let isSuccess = false;
+    while (!isSuccess) {
+      const col = getRandomCoordinate(gameBoard.body.length);
+      const row = getRandomCoordinate(gameBoard.body.length);
+      const isVertical = Math.round(Math.random());
+      const newShip = new Ship(shipLength);
+      isSuccess = gameBoard.placeShip({ x: col, y: row, isVertical }, newShip);
+    }
+  });
+};
+
+const openingModal = (player, board, computer) => {
+  const ships = {
+    Carrier: 5,
+    Battleship: 4,
+    Destroyer: 3,
+    Submarine: 3,
+    "Patrol Boat": 2,
+  };
+
   const modal = createModal();
-  // Create model;
   modal.innerHTML = `
   <div class="modal-content">
   <p>Welcome to Battleship</p>
@@ -75,17 +90,12 @@ const openingModal = (player, board) => {
   </div>
   </div>
   `;
+
   const shipsContainer = modal.querySelector(".ships");
-  const ships = {
-    Carrier: 5,
-    Battleship: 4,
-    Destroyer: 3,
-    Submarine: 3,
-    "Patrol Boat": 2,
-  };
 
   const shipElements = Object.entries(ships).map(makeShipElement);
   shipsContainer.append(...shipElements);
+
   const rotate = modal.querySelector("button");
   rotate.addEventListener("click", () => {
     shipElements.forEach((ship) => {
@@ -93,12 +103,6 @@ const openingModal = (player, board) => {
       ship.dataset.isVertical = Number(ship.dataset.isVertical) ? 0 : 1;
     });
   });
-
-  // * Add function that will populate ships on the player side! Make it remove the ship
-  // * (Partial) Ship names and Instruction on Modal Content
-  // * If there are no ships left, Remove the Modal Content and Start the Game!
-  // TODO Populate Computer Board with random ships
-  // TODO Refactor!
 
   const gb = generateBoard(player.gb.body);
   gb.forEach((cell) => {
@@ -132,6 +136,7 @@ const openingModal = (player, board) => {
         },
         new Ship(length)
       );
+
       if (!isShipSuccessful) return;
       const myCoord = gb.filter((cell) => {
         const [y, x] = JSON.parse(cell.dataset.coordinates);
@@ -141,9 +146,11 @@ const openingModal = (player, board) => {
           }
         }
       });
-      // Dispplay the ships
+
+      // Display the ships
       myCoord.forEach((ship) => {
         ship.style.backgroundColor = "white";
+        // Displays the ships on the main gameBoard
         board.find(
           (mainShip) =>
             mainShip.dataset.coordinates === ship.dataset.coordinates
@@ -155,28 +162,31 @@ const openingModal = (player, board) => {
         `[data-ship-name="${shipName}"`
       );
       shipNode.remove();
+
       // Check if Ships are still present on ship
       if (!shipsContainer.querySelector(".ship")) {
         modal.remove();
       }
     });
   });
+
   modal.querySelector(".mini-gameboard").append(...gb);
   document.body.appendChild(modal);
-  // TODO Make the Placement of Random ships on Computer Board!
+
+  placeRandomShips(computer.gb, ships);
 };
 
 const endGame = (playerWin) => {
   const modal = createModal();
   modal.innerHTML = `
      <div class="modal-content">
-     <p>${playerWin ? "You win!" : "Comp win!"}</p>
+     <p>${playerWin ? "You wins!" : "Computer wins!"}</p>
      <button>Play Again</button>
    </div>
      `;
   modal.querySelector("button").addEventListener("click", () => {
-    gameLoop();
     modal.remove();
+    gameLoop();
   });
   document.body.appendChild(modal);
 };
@@ -194,7 +204,7 @@ const gameLoop = () => {
   const p1 = new Player("p1");
   const bot = new Player("bot", true);
   const newBoard = generateBoard(p1.gb.body);
-  openingModal(p1, newBoard);
+  openingModal(p1, newBoard, bot);
 
   //   p1.gb.placeShip({ x: 0, y: 0 }, new Ship());
   //   bot.gb.placeShip({ x: 1, y: 1 }, new Ship());
@@ -211,7 +221,6 @@ const gameLoop = () => {
 
     // Enemy Comp
     const { coord: enemyCoord, hit: enemySuccess } = bot.attackRandomly(p1);
-    // console.log(JSON.stringify(enemyCoord));
     const attackedCell = newBoard.find(
       (div) => div.dataset.coordinates === JSON.stringify(enemyCoord)
     );
